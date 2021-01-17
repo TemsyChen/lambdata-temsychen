de7d7c0c573d71996302b53a15e0c3b0bb0c57e6
TemsyChen <70554318+TemsyChen@users.noreply.github.com>
2021-01-16 17:09:21 -0800

    PEP8 hw

--- lambdata_temsychen/helper_functions.py
+++ lambdata_temsychen/helper_functions.py
@@ -2,11 +2,13 @@
 
 import pandas as pd
 from sklearn.model_selection import train_test_split
+from sklearn.utils import shuffle
 
 def null_count(df):
 #check dataframe for nulls and return number of missing values
 
-    print(df.isna().sum())
+    print(df.isna().sum().sum())
+  
 
 def train_split(df, frac):
 #respectively returns training pd.Dataframe and test pd.Dataframe
@@ -16,3 +18,16 @@ def train_split(df, frac):
     return X_train, X_test
 
 
+def randomize(df, seed):
+    df = shuffle(df, random_state=seed)
+
+    return(df)
+
+
+if __name__ == '__main__':
+    print('hi temsy')
+    df = pd.read_csv('https://archive.ics.uci.edu/ml/machine-learning-databases/abalone/abalone.data')
+    print('Original df:\n', df.head())
+    print('Null count:', null_count(df))
+    print('Train test split:', train_split(df, .8))
+    print('Randomize:', randomize(df, 37))
